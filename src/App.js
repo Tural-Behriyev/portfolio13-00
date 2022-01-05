1b7caa6aace97e3ad1fb1da6f43506bef96cae59
Behriyev <turalbaxriyev@gmail.com>
2022-01-05 13:32:30 +0400

    Navbar has been compling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 // compenentler bura gelecek
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
-     <h1>Hello World</h1>
      <Navbar />
     </>
   );
